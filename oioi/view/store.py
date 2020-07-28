23b79384195a1c3c0174e195da2577290b33fc7b
sineunju <eunjuoi0515@gmail.com>
2020-07-29 08:45:04 +0900

    [FIX] 토큰 삭제

--- oioi/view/store.py
+++ oioi/view/store.py
@@ -1,12 +1,10 @@
 from flask_restful import Resource
-from flask_jwt_extended import jwt_required
 
 from controller.store import store_detail
 
 
 class Store(Resource):
 
-    @jwt_required
     def get(self, store_id):
 
         return store_detail(store_id)

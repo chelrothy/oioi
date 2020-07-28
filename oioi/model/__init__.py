b8751bf32abf2630f16d5c570c54f955ebdb7ecc
sineunju <eunjuoi0515@gmail.com>
2020-07-28 20:16:45 +0900

    [FIX] 수정

--- oioi/model/__init__.py
+++ oioi/model/__init__.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"), encoding="utf-8", pool_size=20,
-                       pool_recycle=500, max_overflow=20)
+engine = create_engine(os.getenv("DATABASE_URL"), encoding="utf-8")
 
 Base = declarative_base()
 

7a534ff7629d1ac2d437c6d02795eb74ca2a0421
sineunju <eunjuoi0515@gmail.com>
2020-07-28 20:17:57 +0900

    [TEST]

--- oioi/model/__init__.py
+++ oioi/model/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+print(os.getenv("DATABASE_URL"))
 engine = create_engine(os.getenv("DATABASE_URL"), encoding="utf-8")
 
 Base = declarative_base()

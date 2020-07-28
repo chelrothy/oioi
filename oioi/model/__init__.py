f8f00be7834c7fcc493bae6481e84dde7e9c7b64
sineunju <eunjuoi0515@gmail.com>
2020-07-28 20:19:39 +0900

    [FIX] 테스트용 삭제

--- oioi/model/__init__.py
+++ oioi/model/__init__.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-print(os.getenv("DATABASE_URL"))
 engine = create_engine(os.getenv("DATABASE_URL"), encoding="utf-8")
 
 Base = declarative_base()
